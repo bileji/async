c213a84cbba8c922cf5f58daae8104da27298aca
shuchao <645904877@qq.com>
2016-05-31 17:36:45 +0800

    add run method

---
+++ app/Worker/Sms.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * this source file is Sms.php
+ *
+ * author: shuc <shuc324@gmail.com>
+ * time:   2016-05-31 17-13
+ */
+namespace Worker;
+
+class Sms extends Worker
+{
+
+}
--- app/Worker/Worker.php
+++ app/Worker/Worker.php
@@ -7,12 +7,23 @@
  */
 namespace Worker;
 
+use App\Utils\Logger;
+use Exception;
+use GearmanJob;
+
 class Worker
 {
-    public function run($job, &$log)
+    public function run(GearmanJob $job, &$log)
     {
-        $log[] = "start";
-        echo "hello";
-        echo var_export($job, true);
+        $workload = json_decode($job->workload(), true);
+
+        try {
+            $workload['method'] = empty($workload['method']) ? 'progress' : $workload['method'];
+            Logger::info(__CLASS__ . '->' . $workload['method'] . ' got workload.', $workload);
+            $result = call_user_func_array([$this, $workload['method']], [$workload, $log]);
+            Logger::info('result of the ' . __CLASS__ . '->' . $workload['method'] . '.', $result);
+        } catch (Exception $e) {
+            Logger::emerg('class ' . __CLASS__ . ' throw exception:' . $e->getMessage(), $workload);
+        }
     }
 }
